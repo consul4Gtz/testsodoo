2080bd0120871dc14712942765381185e0a383e8
consul4Gtz <consultor4@itechnologysol.com>
2024-05-17 14:31:06 -0600

    segunda modificacion  unicode y funcion en modelo

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from unidecode import unidecode
 
 class my_module(models.Model):
     _name = 'my_module.my_module'
@@ -12,6 +13,16 @@ class my_module(models.Model):
     #cambios realizados 
     start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
 
+    @api.model  
+    def create(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+            return super(my_module, self).create(values)
+    def write(self, values):
+        if 'name' in values:
+            values['name'] = unidecode(values['name'])
+            return super(my_module, self).write(values)
+        
     @api.depends('value')
     def _value_pc(self):
         self.value2 = float(self.value) / 100
