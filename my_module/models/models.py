55321d98215f2d804a91ee2d64c5ca86d9e8be59
consul4Gtz <consultor4@itechnologysol.com>
2024-05-17 15:50:58 -0600

    prueba en templates

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
 from unidecode import unidecode
+from odoo import models, fields, api
+
 
 class my_module(models.Model):
     _name = 'my_module.my_module'
@@ -12,16 +12,17 @@ class my_module(models.Model):
     value2 = fields.Float("Cantidad",compute="_value_pc", store=True)
     #cambios realizados 
     start_datetime = fields.Datetime('Tiempo inicial', default=lambda self: fields.Datetime.now())
-
+    
     @api.model  
     def create(self, values):
         if 'name' in values:
             values['name'] = unidecode(values['name'])
             return super(my_module, self).create(values)
-    def write(self, values):
-        if 'name' in values:
-            values['name'] = unidecode(values['name'])
-            return super(my_module, self).write(values)
+        
+        def write(self, values):
+            if 'name' in values:
+                values['name'] = unidecode(values['name'])
+                return super(my_module, self).write(values)
         
     @api.depends('value')
     def _value_pc(self):
