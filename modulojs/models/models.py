d42591e993f27fd8ad79fb934e5040367187f61b
consul4Gtz <consultor4@itechnologysol.com>
2024-05-29 16:45:41 -0600

    Delete modulojs directory

--- modulojs/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class MiModelo(models.Model):
-    _name = 'mi_modulo.mi_modelo'
-    _description = 'Mi Modelo'
-
-    mi_campo = fields.Char(string="Mi Campo")
